a81bf3bae6ef603437999520ad9788dbb3c6f6b7
xlccccc <2595251998@qq.com>
2023-03-30 23:55:48 +0800

    update

--- pypicgo/plugins/typora/__init__.py
+++ pypicgo/plugins/typora/__init__.py
@@ -14,4 +14,8 @@ class TyporaPlugin(FinallyPlugin):
                 urls.append(result.remote_url)
         if len(urls) > 0:
             message = os.linesep.join(urls)
-            sys.stdout.write(f'Upload Success:{os.linesep}{message}{os.linesep}')
+            sys.stdout.write(f'Upload Success:{os.linesep}message{os.linesep}')
+            lines = [line for line in message.splitlines() if line.strip()]
+            f =  open('url.txt', 'w')
+            f.write('\n'.join(lines))
+            f.close()
--- pypicgo/upload.py
+++ pypicgo/upload.py
@@ -3,7 +3,7 @@ import argparse
 from pypicgo.core.config import Settings
 from pypicgo.core.execute import create_uploader
 from pypicgo.core.logger import logger
-
+from pypicgo.core.base.plugin import BeforePlugin, AfterPlugin, FinallyPlugin
 
 def action():
 
@@ -13,10 +13,18 @@ def action():
         description='Welcom to PyPicGo'
         )
     parser.add_argument('-n', '--name', type=str, help="uploader name", metavar="github")
-    parser.add_argument('-f', '--files', nargs='+', required=True, type=str, help="upload files list", metavar="./img.png")
+    # Aparser.add_argument('-f', '--files', nargs='+', required=True, type=str, help="upload files list", metavar="./img.png")
+    parser.add_argument('-m', '--mdfiles')
     args = parser.parse_args()
     uploader_name = args.name
-    files = args.files
+    markdown_file = args.mdfiles
+    mdFileRead1 = open(markdown_file, 'r', encoding='utf-8')
+    mdFileReadText = mdFileRead1.readlines()
+    mdFileRead1.close()
+    mdFileRead2 = open(markdown_file, 'r', encoding='utf-8')
+    mdFileWriteText = mdFileRead2.read()
+    mdFileRead2.close()
+    files = mdFileFindImg(mdFileReadText)
     settings = Settings(uploader_name=uploader_name)
     uploader = settings.uploader_class
     uploader_config = settings.uploader_config
@@ -25,9 +33,35 @@ def action():
         logger.info('upload start')
         for filepath in files:
             logger.info(f'upload file [{filepath}]')
+        
             uploader.do(filepath)
+        f = open('url.txt', 'r')
+        newFilePaths = f.readlines()
+        for filepath, newfilepath in zip(files, newFilePaths):
+            mdFileWriteText = mdFileChange(mdFileWriteText, filepath, newfilepath)
+        mdFileWrite = open(markdown_file, 'w', encoding='utf-8')
+        mdFileWrite.write(mdFileWriteText)
+        mdFileWrite.close()
         logger.info('all file has been handled')
 
+def mdFileFindImg(mdFile):
+    files = []
+    for line in mdFile:
+        if line.find('![image-') != -1:
+            start = line.find('(')
+            end = line.find(')', start)
+            files.append(line[start+1:end])
+    return files
+
+
+
+def mdFileChange(mdFile, oldFilepath, newFilepath):
+    if mdFile.find(oldFilepath) != -1:
+        potision = mdFile.find(oldFilepath)
+        start = mdFile.find('![image-', potision - 30, potision)
+        end  = mdFile.find(')', potision)     
+        mdFile = mdFile.replace(mdFile[start:end + 1], '![](' + newFilepath + ')')
+    return mdFile
 
 if __name__ == '__main__':
     action()
